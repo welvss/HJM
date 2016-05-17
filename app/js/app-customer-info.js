2e484e0ac965f131c6574485b2240438d5dbb254
Welvin Medina <welvs.medina@gmail.com>
2016-05-17 11:14:07 +0800

    Customer

---
+++ app/js/app-customer-info.js
@@ -0,0 +1,64 @@
+$( document ).ready(function() {
+ $(".mode").click(function(){
+  	$('.ui.modal').modal('show');
+  });
+  $('.menu .item')
+  .tab()
+;
+$('#same-as').change(function(){
+    if (this.checked) {
+        document.getElementById("ship-street").disabled = true;
+		document.getElementById("ship-city").disabled = true;
+		document.getElementById("ship-baranggay").disabled = true;
+    }
+	else{
+		 document.getElementById("ship-street").disabled = false;
+		document.getElementById("ship-city").disabled = false;
+		document.getElementById("ship-baranggay").disabled = false;
+	}
+
+
+});
+// Initialize sidebar
+$('.sidebar')
+    .sidebar({
+      dimPage: true,
+      closable: true
+    });
+ $('.right.menu.open').on("click",function(e){
+    e.preventDefault();
+    $('.ui.vertical.menu').toggle();
+  });
+    
+  $('.ui.dropdown').dropdown();
+
+$(".sidebar-button").click(function(){
+    $('.sidebar')
+  .sidebar('toggle')
+;
+  });
+ $('#case-history').DataTable( {
+        "paging":         false,
+        'aoColumnDefs': [{
+        'bSortable': false,
+        'aTargets': [-1, -2, -3] /* 1st one, start by the right */
+    }]
+    } );
+  $('#transaction-history').DataTable( {
+        "paging":         false,
+        'aoColumnDefs': [{
+        'bSortable': false,
+        'aTargets': [-1, -2, -3] /* 1st one, start by the right */
+    }]
+    } );
+
+  var dataTable = $('#customer-table').dataTable();
+    $("#search-customer").keyup(function() {
+        dataTable.fnFilter(this.value);
+    });    
+
+    $('.popup')
+  .popup()
+;
+
+});
